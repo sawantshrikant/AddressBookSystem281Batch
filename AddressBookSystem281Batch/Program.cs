845e5ebb3cedf60311b90d38c5eadc323f99f8be
Shrikant <sawantshrikant07@gmail.com>
2023-06-01 11:33:50 +0530

    [Shrikant] add:WelcomeMeassage

---
+++ AddressBookSystem281Batch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBookSystem281Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program!");
+        }
+
+
+    }
+}
